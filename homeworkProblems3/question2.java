ae6bdb4a607f634458f13e6b802bbb047368ebd9
ChaitanyaBhatt147 <90873940+ChaitanyaBhatt147@users.noreply.github.com>
2023-06-20 09:54:42 +0530

    Delete homeworkProblems3 directory

--- homeworkProblems3/question2.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class question2 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        for(;;){
-            System.out.println("Chaitanya Bhatt");
-        }
-    }
-}
